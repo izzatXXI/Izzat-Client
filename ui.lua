8a8f11b3c9ed48af6bf7c242cc264a35be577600
izzatXXI <goelele123@gmail.com>
2024-10-24 18:54:50 +0100

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,3 +1,15 @@
 local ui = {}
 
+function ui.new()
+    local gui = setmetatable({},{})
+    gui.__index = gui
+    gui.MAIN = Instance.new("ScreenGui",game.Players.LocalPlayer.PlayerGui)
+    function gui:button(self,name,callback)
+        local button = Instance.new("TextButton",gui.MAIN)
+        button.Text = name
+        button.MouseButton1Click:Connect(callback)
+        button.Parent = self.MAIN
+    end
+end
+
 return ui
