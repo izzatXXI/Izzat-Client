6353655d47e10e0435f478de893b5c301b9aab1a
izzatXXI <goelele123@gmail.com>
2024-10-26 14:22:07 +0100

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -69,7 +69,7 @@ function ui:new(parent)
             button.Parent = self.frame
             buttonmm.btn = button
             buttonmm.value = false
-            button.BackgroundColor3 = buttonmm.value
+            button.BackgroundColor3 = buttonColors[tostring(buttonmm.value)]
             buttonmm.keybind = keybind or nil
             return buttonmm
         end
@@ -82,5 +82,3 @@ function ui:new(parent)
     end)
     return gui
 end
-
-return ui
